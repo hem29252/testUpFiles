66efe623790f9e4732c174c570feeb7c9d1dd45d
hem29252 <hem29252@gmail.com>
2021-07-09 00:45:09 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const fs = require('fs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '15MB' }))
+
+app.post('/', (req, res) => {
+	fs.writeFile('./out.png', req.body.imgsource, 'base64', (err) => {
+		if (err) throw err
+	})
+	res.status(200)
+})
+app.listen(8080)
